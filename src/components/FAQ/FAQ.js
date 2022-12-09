cad42a9ae17bd15d3c0f414cb7f6e6c0a6a0d01c
revyrob <kayle.robson@gmail.com>
2022-12-09 13:39:05 -0800

    finished the info section and added the faq section

---
+++ src/components/FAQ/FAQ.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+function FAQ() {
+  return (
+    <section id="faq">
+      <h1 className="text-2xl p-2 border-b-2 border-gray-500 font-creepster">
+        FAQ
+      </h1>
+      <div className="w-max-3/4 my-6">
+        <h2 className="text-lg font-oswald">Where can I get the audio?</h2>
+        <p className="text-base">
+          We are hosting the audio on Spotify. You can sign-up for Spotify for
+          free and get it there.
+        </p>
+      </div>
+      <div className="w-max-3/4 my-6">
+        <h2 className="text-lg font-oswald">How long is the audio tour?</h2>
+        <p className="text-base">60 to 90 minutes.</p>
+      </div>
+      <div className="w-max-3/4 my-6">
+        <h2 className="text-lg font-oswald">Do I need to do all the stops?</h2>
+        <p className="text-base">
+          No, there is no order and you are not required to do all the stops
+          with the audio.
+        </p>
+      </div>
+      <div className="w-max-3/4 my-6">
+        <h2 className="text-lg font-oswald">How long is the audio tour?</h2>
+        <p className="text-base">60 to 90 minutes.</p>
+      </div>
+      <div className="w-max-3/4 my-6">
+        <h2 className="text-lg font-oswald">How much does it cost?</h2>
+        <p className="text-base">
+          It is free! We do appreciate you to visit our youTube channel, like
+          the audio on Spotify, and give us a review on Trip Advisor and/or
+          Google.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default FAQ;
