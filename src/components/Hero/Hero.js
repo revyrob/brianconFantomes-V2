42de8116675d9d163bcb712b695e15e75e5adb37
revyrob <kayle.robson@gmail.com>
2022-12-21 14:21:48 -0800

    worked on carousel

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,7 +13,7 @@ function Hero() {
         class="carousel slide relative mt-12"
         data-bs-ride="carousel"
       >
-        <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
+        <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4 ">
           <button
             type="button"
             data-bs-target="#carouselExampleCaptions"
@@ -37,17 +37,25 @@ function Hero() {
         </div>
         <div className="carousel-inner relative w-full overflow-hidden">
           <div className="carousel-item active relative float-left w-full">
-            <img src={book} className="block w-full" alt="briancon" />
+            <img
+              src={book}
+              className="block w-full mt-12 md:w-11/12 mx-auto md:mt-0"
+              alt="briancon"
+            />
           </div>
-          <div className="carousel-item relative float-left w-full">
+          <div className="carousel-item relative float-left w-full ">
             <img
               src={briancon}
-              className="block w-full"
+              className="block w-full mt-12 md:w-11/12 mx-auto md:mt-0"
               alt="book and headphones"
             />
           </div>
-          <div className="carousel-item relative float-left w-full">
-            <img src={gargouille} className="block w-full" alt="snowBriancon" />
+          <div className="carousel-item relative float-left w-full ">
+            <img
+              src={gargouille}
+              className="block w-full mt-12 md:w-11/12 mx-auto md:mt-0"
+              alt="snowBriancon"
+            />
           </div>
         </div>
         <button
@@ -57,13 +65,13 @@ function Hero() {
           data-bs-slide="prev"
         >
           <span
-            className="carousel-control-prev-icon inline-block bg-no-repeat"
+            className="carousel-control-prev-icon inline-block bg-no-repeat "
             aria-hidden="true"
           ></span>
           <span className="visually-hidden">Previous</span>
         </button>
         <button
-          className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+          className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0 "
           type="button"
           data-bs-target="#carouselExampleCaptions"
           data-bs-slide="next"
