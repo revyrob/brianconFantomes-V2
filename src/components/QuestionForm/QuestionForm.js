1dffd60b8de6526c35c2fefae37605f0ae903819
revyrob <kayle.robson@gmail.com>
2023-01-24 15:27:09 -0800

    working on emailjs

--- src/components/QuestionForm/QuestionForm.js
+++ src/components/QuestionForm/QuestionForm.js
@@ -1,8 +1,12 @@
 import { Formik, Form } from "formik";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 function QuestionForm() {
+  const form = useRef();
+
   //event handler for creating new comment
   const handleComment = (event) => {
     event.preventDefault();
@@ -12,6 +16,22 @@ function QuestionForm() {
     }
   };
 
+  const SERVICE_ID = process.env.SERVICE_ID;
+  const TEMPLATE_ID = process.env.TEMPLATE_ID;
+  const PUBLIC_KEY = process.env.PUBLIC_KEY;
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY).then(
+      (result) => {
+        console.log(result.text);
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
+  };
   return (
     <div
       id="contact"
@@ -24,7 +44,7 @@ function QuestionForm() {
           alert(JSON.stringify(values, null, 2));
         }}
       >
-        <Form className="form" method="post" onSubmit={handleComment}>
+        <Form className="form" method="post" onSubmit={sendEmail}>
           <div className="form__form flex flex-col justify-between h-72 ">
             <TextField name="name" required fullWidth id="name" label="Name" />
             <TextField
