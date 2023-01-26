baeb944865d3114d93f6b2b7c4b70c8fba4e8220
revyrob <kayle.robson@gmail.com>
2023-01-25 22:09:38 -0800

    addeding scrolling affect for credits

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,29 +17,17 @@ module.exports = {
         "chevron-down": "url('/assets/icons/chevron-down.svg')",
       },
       keyframes: {
-        wave: {
-          "0%": { transform: "translateY(0)" },
-          "10%": { transform: "translateY([-100px])" },
-          "20%": { transform: "translateY([-200px])" },
-          "30%": { transform: "translateY([-300px])" },
-          "40%": { transform: "translateY([-400px])" },
-          "50%": { transform: "translateY([-500px])" },
-          "60%": { transform: "translateY([-600px])" },
-          "100%": { transform: "translateY([-700px])" },
-        },
         marquee: {
           "0%": { transform: "translateY(0%)" },
           "100%": { transform: "translateY(-100%)" },
         },
         marquee2: {
-          "0%": { transform: "translateY(100%)" },
-          "100%": { transform: "translateY(0%)" },
+          "0%": { transform: "translateY(30%)" },
+          "100%": { transform: "translateY(-100%)" },
         },
       },
       animation: {
-        "waving-hand": "wave 2s linear infinite",
-
-        "marquee-infinite": "marquee 20s linear infinite",
+        "marquee-infinite": "marquee2 20s linear infinite",
       },
     },
   },
