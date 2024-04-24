a0c5fe5a214f4594079e0cf4f336b9863666c995
revyrob <kayle.robson@gmail.com>
2024-04-23 22:08:47 -0700

    trying to test the formik

--- src/components/QuestionForm/QuestionForm.js
+++ src/components/QuestionForm/QuestionForm.js
@@ -1,78 +1,77 @@
 import { Formik, Form } from "formik";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import React, { useRef } from "react";
+import React from "react";
 import emailjs from "@emailjs/browser";
 
 function QuestionForm() {
-  const form = useRef();
+  const form = React.useRef();
 
-  //event handler for creating new comment
-  const handleComment = (event) => {
-    event.preventDefault();
-    const question = event.target.question.value;
-    if (question !== "") {
-      event.preventDefault();
-    }
+  const sendEmail = (values, { resetForm }) => {
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          resetForm();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
 
-  const SERVICE_ID = process.env.SERVICE_ID;
-  const TEMPLATE_ID = process.env.TEMPLATE_ID;
-  const PUBLIC_KEY = process.env.PUBLIC_KEY;
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY).then(
-      (result) => {
-        console.log(result.text);
-      },
-      (error) => {
-        console.log(error.text);
-      }
-    );
-  };
   return (
     <div
       id="contact"
       className="bg-slate-50 p-4 flex flex-col justify-around rounded-md"
     >
       <Formik
-        initialValues={{ question: "" }}
-        onSubmit={async (values) => {
-          await new Promise((resolve) => setTimeout(resolve, 500));
-          alert(JSON.stringify(values, null, 2));
-        }}
+        initialValues={{ name: "", email: "", question: "" }}
+        onSubmit={sendEmail}
       >
-        <Form className="form" method="post" onSubmit={sendEmail}>
-          <div className="form__form flex flex-col justify-between h-72 ">
-            <TextField name="name" required fullWidth id="name" label="Name" />
-            <TextField
-              name="email"
-              required
-              fullWidth
-              id="email"
-              label="Email"
-            />
-            <TextField
-              name="question"
-              required
-              fullWidth
-              id="question"
-              label="Question"
-            />
-
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              style={{ backgroundColor: "#fcaf34", color: "white" }}
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Submit
-            </Button>
-          </div>
-        </Form>
+        {({ isSubmitting }) => (
+          <Form ref={form} className="form" method="post">
+            <div className="form__form flex flex-col justify-between h-72">
+              <TextField
+                name="name"
+                required
+                fullWidth
+                id="name"
+                label="Name"
+              />
+              <TextField
+                name="email"
+                required
+                fullWidth
+                id="email"
+                label="Email"
+              />
+              <TextField
+                name="question"
+                required
+                fullWidth
+                id="question"
+                label="Question"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                style={{ backgroundColor: "#fcaf34", color: "white" }}
+                sx={{ mt: 3, mb: 2 }}
+                disabled={isSubmitting}
+              >
+                {isSubmitting ? "Submitting..." : "Submit"}
+              </Button>
+            </div>
+          </Form>
+        )}
       </Formik>
     </div>
   );
