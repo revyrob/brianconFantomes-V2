c4b57277ffc503f4aa1073ac74983a5fee5f0ea3
revyrob <kayle.robson@gmail.com>
2025-01-26 13:03:07 -0800

    make a onclick over the achetez button

--- src/components/AudioBookLink/AudioBookLink.js
+++ src/components/AudioBookLink/AudioBookLink.js
@@ -1,10 +1,17 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { LanguageContext } from "../../Language";
 import chevron from "../../assets/icons/chevron-down.svg";
 
 function AudioBookLink() {
   const { dictionary } = useContext(LanguageContext);
-  console.log(dictionary.book);
+  //if isShown is false not all questions are displayed.  If true all questions are displayed
+  const [isShown, setIsShown] = useState(false);
+
+  //toggle function for drop down
+  function toggle() {
+    //if selected the question drop down, compare previous state
+    setIsShown((prevCheck) => !prevCheck);
+  }
   return (
     <section
       id="tour"
@@ -23,10 +30,27 @@ function AudioBookLink() {
         <p className="text-white font-oswald text-3l p-2 w-[80%]">
           {dictionary.book.descrip}
         </p>
-        <button className="flex items-center justify-center bg-yellow-200 px-4 py-4 max-w-[210px] rounded-full ">
-          {dictionary.book.btn}
-          <img className="pl-2" src={chevron} alt="chevron down" />
-        </button>
+        <div>
+          <button
+            onClick={toggle}
+            className="flex items-center justify-center bg-yellow-200 px-4 py-4 max-w-[210px] rounded-full "
+          >
+            {dictionary.book.btn}
+            <img className="pl-2" src={chevron} alt="chevron down" />
+          </button>
+          <div
+            className="bg-white flex flex-col max-w-[200px] rounded-xl"
+            style={
+              isShown === true ? { visibility: "visible" } : { display: "none" }
+            }
+          >
+            {dictionary.book.vendors?.map((vendor, index) => (
+              <a className="p-2" key={index} href={vendor.url}>
+                {vendor.name}
+              </a>
+            ))}
+          </div>
+        </div>
       </div>
     </section>
   );
